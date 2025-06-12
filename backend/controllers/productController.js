1b2176b4e8b427f1deb54de3e72ca001874a2f06
Sushant Shrestha <sushant.sht@gmail.com>
2025-06-12 09:49:56 +0545

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,13 @@
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products); 
+};
+
+exports.createProduct = async (req, res) => {
+    const newProduct = new Product(req.body);
+    const saved = await newProduct.save();
+    res.status(201).json(saved);
+};
+
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+// app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+app.use(express.json());
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+     .then(() => console.log('MongoDB connected'))
+     .catch((err) => console.error(err));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
